7db2a8899257c7e83dc84bd8379eacde1ba001b7
Arttu <arttu.otava@gmail.com>
2021-04-06 11:53:48 +0300

    changed timigs

--- project_B4023/core.py
+++ project_B4023/core.py
@@ -410,7 +410,7 @@ class write_setpoint:
 
                 output = []
                 output.append((ROOM_SETPOINT, setpoint))
-                output.append(("External Control", 1))
+                #output.append(("External Control", 1))
 
                 if (datetime.now().hour == heating_off_at_hour and heating_is_off != True \
                                                     and is_heating_control_allowed()):
