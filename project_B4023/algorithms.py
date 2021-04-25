67ff7f5a00bec117b6e243ceda7592463880c831
Arttu <arttu.otava@gmail.com>
2021-04-25 12:51:54 +0300

    added mse to pred calc B4023

--- project_B4023/algorithms.py
+++ project_B4023/algorithms.py
@@ -184,7 +184,7 @@ def ML_model2(room_temp,old_setpoint, current_temp, temp_1hour, temp_2hour, temp
     inter = filename.intercept_
     pred = inter + weight[0] *  old_setpoint + weight[1] * current_temp + weight[2] \
             * temp_1hour + weight[3] * temp_2hour + weight[4] * temp_4hour
-    mse = np.square(np.subtract(room_temp,pred)).mean()
+    mse = np.mean((room_temp-pred)**2)
     print(pred)
     print(mse)
     print(weight)
